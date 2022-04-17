55b43c4735618e457317440986ac5deef4efd4c6
1KB <290111336@qq.com>
2022-04-17 16:43:45 +0800

    feat(seo):add seo information

---
+++ src/data/seoConfig.ts
@@ -0,0 +1,54 @@
+import { makeStrings } from '@monoid-dev/use-strings';
+
+export const useDescriptionStrings = makeStrings({
+  'en-US': {
+    home: 'Monoid is a passionate and dynamic technology company engaged in software and hardware development.',
+    company:
+      'With effective and rigid new techniques, we clear the path of old obstructions and pave the way to the future.',
+    works: 'Display of works accumulated by monoid for a long time.',
+    recruit:
+      'Monoid is an oasis of talented engineers and creators. Here are the recruitment details.',
+    contact: 'Monoid contact address and contact information.',
+  },
+  'zh-CN': {
+    home: 'Monoid 是一个有激情有活力的从事软件硬件开发的科技公司。',
+    company:
+      'Monoid 通过有效而严格的新技术，清除障碍壁垒，用探索精神为未来铺平道路。',
+    works: 'Monoid 长久以来所积累的作品展示。',
+    recruit: 'Monoid 是天才工程师和创造者的绿洲，以下是招募详细。',
+    contact: 'Monoid 的联系地址以及联系方式。',
+  },
+  'ja-JP': {
+    home: 'Monoidは情熱的で活気のあるソフトウェアハードウェア開発に従事する科学技術会社です。',
+    company:
+      'Monoidは有効で厳格な新技術を通じて、障害の障壁を取り除き、探索精神で未来のために道を舗装した。',
+    works: 'Monoidが長い間積み重ねてきた作品展示。',
+    recruit:
+      'Monoidは天才エンジニアと創造者のオアシスで、以下は募集の詳細です。',
+    contact: 'Monoidの連絡先と連絡先。',
+  },
+});
+
+export const useKeywordsStrings = makeStrings({
+  'en-US': {
+    home: 'Monoid,technology company,Internet plus solution,programming development',
+    company: 'Monoid,Monoid company profile',
+    works: 'Monoid,Monoid works',
+    recruit: 'Monoid,Monoid recruit',
+    contact: 'Monoid,Monoid contact information',
+  },
+  'zh-CN': {
+    home: 'Monoid,科技公司,互联网+解决方案,程序设计开发',
+    company: 'Monoid,Monoid简介',
+    works: 'Monoid,Monoid作品',
+    recruit: 'Monoid,Monoid招募',
+    contact: 'Monoid,Monoid联络',
+  },
+  'ja-JP': {
+    home: 'Monoid,科学技術会社,インターネット+ソリューション,プログラム設計開発',
+    company: 'Monoid,Monoidの概要',
+    works: 'Monoid,Monoid作品',
+    recruit: 'Monoid，Monoid募集',
+    contact: 'Monoid，Monoid連絡',
+  },
+});
