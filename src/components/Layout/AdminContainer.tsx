4d27d248d4fd6e118b8334e0364c179621d4d132
hanayashiki <cwang592@usc.edu>
2022-05-03 12:07:57 +0900

    feat: make admin usable in mobile mode

--- src/components/Layout/AdminContainer.tsx
+++ src/components/Layout/AdminContainer.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Cookies from 'js-cookie';
 import Link from 'next/link';
 
@@ -25,11 +26,21 @@ export const AdminContainer: React.FC = (props) => {
   };
 
   return (
-    <div className="flex flex-1 shrink min-h-0 gap-x-[2rem]">
-      <div className="flex flex-col w-[300px] ml-[2rem] my-[2rem] border-r border-black text-[20px]">
+    <div
+      className={clsx('flex flex-1 shrink min-h-0 gap-x-[2rem]', 'sm:flex-col')}
+    >
+      <div
+        className={clsx(
+          'flex flex-col w-[300px] ml-[2rem] my-[2rem] border-r border-black text-[20px]',
+        )}
+      >
         <h2 className="font-bold">Admin</h2>
         {renderNavigationButton('Recruit', '/admin/recruit')}
         {renderNavigationButton('Contact', '/admin/contact')}
+        {renderNavigationButton(
+          'Analytics',
+          'https://plausible.monoid.co.jp/monoid.co.jp',
+        )}
         {renderNavigationButton('Log out', '/admin/login')}
       </div>
       <div className="flex-1 flex-col overflow-auto">{children}</div>
